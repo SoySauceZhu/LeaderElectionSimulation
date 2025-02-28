71f51e0246b831507b843a5ab0748c0316a57e27
Mingjie.Zhu <zhumingjie0627@outlook.com>
2025-02-28 23:56:53 +0000

    v1.1

--- src/util/GenerateNodes.java
+++ src/util/GenerateNodes.java
@@ -8,6 +8,8 @@ import entity.lcr.LCRNode;
 import java.util.ArrayList;
 import java.util.List;
 
+import static entity.common.Port.RIGHT;
+
 public class GenerateNodes {
 
     public static List<Node> generateHSNodes(int numberOfNodes) {
@@ -27,14 +29,16 @@ public class GenerateNodes {
             Node currentNode = nodes.get(i);
             Node nextNode = nodes.get((i + 1) % nodes.size());
             Node prevNode = nodes.get((i - 1 + nodes.size()) % nodes.size());
-            currentNode.setNeighbor(Port.RIGHT, nextNode);
+            currentNode.setNeighbor(RIGHT, nextNode);
             currentNode.setNeighbor(Port.LEFT, prevNode);
         }
 
         return nodes;
     }
 
-    public static List<Node> generateLCRNodes(int numberOfNodes) {
+
+
+    public static List<Node> generateRandomLCRNodes(int numberOfNodes) {
         List<Node> nodes = new ArrayList<>();
         int[] ids = new int[numberOfNodes];
 
@@ -42,27 +46,35 @@ public class GenerateNodes {
             ids[i] = i;
         }
 
+        // Shuffle the ids array to randomize node IDs
+        java.util.Collections.shuffle(java.util.Arrays.asList(ids));
+
         for (int i = 0; i < numberOfNodes; i++) {
-            Node node = new LCRNode(i);
+            Node node = new LCRNode(ids[i]);
             nodes.add(node);
         }
 
         for (int i = 0; i < numberOfNodes; i++) {
             Node currentNode = nodes.get(i);
             Node nextNode = nodes.get((i + 1) % nodes.size());
-            currentNode.setNeighbor(Port.RIGHT, nextNode);
+            currentNode.setNeighbor(RIGHT, nextNode);
         }
 
         return nodes;
     }
 
     public static void main(String[] args) {
-        List<Node> nodes = GenerateNodes.generateLCRNodes(10);
-        for (Node node : nodes) {
-            System.out.println(node);
-        }
+//        List<Node> nodes = GenerateNodes.generateLCRNodes(10);
+//        List<Node> nodes = GenerateNodes.generateRandomHSNodes(10);
+//        for (Node node : nodes) {
+//            System.out.println(node);
+//        }
 
-        nodes.get(2).start();
+//        Node ptr = nodes.get(0);
+//        while (ptr != null && !ptr.equals(nodes.get(0))) {
+//            System.out.println(ptr);
+//            ptr = ptr.getNeighbors().get(RIGHT);
+//        }
     }
 }
 
