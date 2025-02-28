327cf5fa8191ba22c0f7d80609e6fc6d7a75ae79
Mingjie.Zhu <zhumingjie0627@outlook.com>
2025-02-28 03:38:27 +0000

    minor update

--- src/entity/Node.java
+++ src/entity/Node.java
@@ -7,7 +7,7 @@ public class Node {
     private Integer id;
     private Integer curMaxId;
     private Status status = Status.UNKNOWN;
-    private Node next;
+    private Node next;      // Only sent message to clockwise
     private Integer leaderId;
     private boolean terminated = false;
     private Queue<Message> messageQueue = new LinkedList<>(); // Stores incoming messages
@@ -19,9 +19,6 @@ public class Node {
         this.curMaxId = id; // Initially, each node sends its own ID
     }
 
-    public void setNext(Node next) {
-        this.next = next;
-    }
 
     public boolean start() {
         sendMessage(new Message(MessageType.ELECTION, curMaxId));
@@ -34,7 +31,6 @@ public class Node {
 
 
     public boolean processMessages() {
-        boolean sentMsg = false;
 
         if (buffer != null && !terminated) {
             Message message = buffer;
@@ -46,26 +42,26 @@ public class Node {
                 if (msgContent > curMaxId) {
                     curMaxId = msgContent;
                     sendMessage(message);
-                    sentMsg = true;
+                    return true;
                 } else if (msgContent.equals(id)) { // If it receives its own ID, it wins
                     status = Status.LEADER;
                     this.leaderId = this.id;
                     System.out.println("Node " + id + " received its own ID. It is the leader.");
                     sendMessage(new Message(MessageType.LEADER_ANNOUNCEMENT, id));
                     terminate();
-                    sentMsg = true;
+                    return true;
                 }
             } else if (msgType == MessageType.LEADER_ANNOUNCEMENT) {
                 status = Status.SUBORDINATE;
                 this.leaderId = msgContent;
                 sendMessage(message);
-                //                System.out.println("Node " + id + " acknowledges Leader " + msgContent);
+                System.out.println("Node " + id + " acknowledges Leader " + msgContent);
                 terminate();
-                sentMsg = true;
+                return true;
             }
         }
 
-        return sentMsg;
+        return false;
     }
 
     public void sendMessage(Message message) {
@@ -74,6 +70,9 @@ public class Node {
         lastSentMessage = message;
     }
 
+    public void setNext(Node next) {
+        this.next = next;
+    }
 
     public void terminate() {
         terminated = true;
